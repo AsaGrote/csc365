e05797eb6c047873e8c203c71c030b843389ebf7
AsaGrote <agrote@calpoly.edu>
2024-04-10 11:49:29 -0700

    adjusted price of green potion

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -24,7 +24,7 @@ def get_catalog():
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
                 "quantity": 1,
-                "price": 48,
+                "price": 5,
                 "potion_type": [0, 100, 0, 0],
             }
         ]
